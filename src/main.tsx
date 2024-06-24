03c9a195fabd9b854e07fa0f3c3db1bf1b034efb
leopoldo.flores28 <leopoldo.flores28@gmail.com>
2024-06-24 14:23:27 -0300

    quitado paralax lib

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { ParallaxProvider } from 'react-scroll-parallax'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ParallaxProvider >
     <React.StrictMode>
       <App />
     </React.StrictMode>,
-  </ParallaxProvider>
 )
