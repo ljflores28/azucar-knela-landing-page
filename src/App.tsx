c69f1f1b4dd009f5134646e93dd380696b219fc5
leopoldo.flores28 <leopoldo.flores28@gmail.com>
2024-07-10 15:25:56 -0300

    agregado boton flotante para WS

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Footer from './components/Footer';
 import useScrollPercentage from './hook/useScrollPercentage';
 import Hero  from './components/Hero';
 import Menu from './components/Menu';
+import { BsWhatsapp } from "react-icons/bs";
 
 const App: React.FC = () => {
 
@@ -12,7 +13,10 @@ const App: React.FC = () => {
 
   return (
     <>
-      <div className=" fixed bg-cover w-full h-screen -z-10 animate-switchBg" />
+      <div id="background" className="fixed bg-cover w-full h-screen -z-10 animate-switchBg" />
+      <a id="float" className="relative cursor-pointer w-full  ">
+        <BsWhatsapp fill='#ffff' className='bg-[#26D366] fixed z-20 object-cover size-14 sm:size-20 shadow-2xl shadow-slate-700 right-0 bottom-0 p-4 m-8 rounded-2xl sm:p-6 focus:ring-4 shadow-lg transform active:scale-75 transition-transform'/>
+      </a>
       <header className="h-[15vh] sm:h-fit">
         <Menu />
       </header>
