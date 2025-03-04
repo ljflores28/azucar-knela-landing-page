3831659b022593f3bb6caa300dd58d69e65e94b1
leopoldo.flores28 <leopoldo.flores28@gmail.com>
2025-03-04 19:25:16 -0300

    agregada funcionalidad de banner publicitario

--- src/components/template/CatalogoDeProductos.tsx
+++ src/components/template/CatalogoDeProductos.tsx
@@ -3,6 +3,25 @@ import Buscador from "../atomos/Buscador"
 import HeaderH1 from "../atomos/HeaderH1"
 import CatalogoClasico from "../CatalogoClasico"
 
+type BannerPromocional = {
+    img: string,
+    alt: string,
+    link: string
+}
+
+const bannersPromocionales : BannerPromocional[] = 
+    [
+        // {
+        //     img:"/images/baners/ejemplo-banner-boda.png",
+        //     alt: "Banner promocional 1",
+        //     link: "https://api.whatsapp.com/send?phone=5491167953259&text=🖐️ Hola! Quisiera saber más sobre las tortas de bodas."
+        // },
+        // {
+        //     img:"/images/baners/ejemplo-banner-boda.png",
+        //     alt: "Banner promocional 1",
+        //     link: "https://api.whatsapp.com/send?phone=5491167953259&text=🖐️ Hola! Quisiera saber más sobre las tortas de bodas."
+        // },
+    ]
 
 const CatalogoDeProductos : React.FC = () => {
 
@@ -11,6 +30,18 @@ const CatalogoDeProductos : React.FC = () => {
     return (
         <div className="flex flex-col gap-4 fade">
             <HeaderH1>Catálogo de tortas clásicas</HeaderH1>
+                <div className="relative flex gap-6 overflow-x-auto snap-x snap-mandatory carrusel-catalogo">
+                {    bannersPromocionales.map((banner, index) => {
+                      return  (
+                        <div key={index} className="snap-center shrink-0 w-[320px]">
+                            <a href={banner.link} target="_blank" rel="noopener noreferrer" className="">
+                                <img src={banner.img} alt={banner.alt} className="object-fill rounded-lg" />
+                            </a>
+                        </div>
+                        )
+                    })
+                }  
+                </div>
             <Buscador handleFilter={setFiltro} />
             <div className="flex flex-wrap items-center justify-center gap-3">
                 <CatalogoClasico filtro={filtro}  />
